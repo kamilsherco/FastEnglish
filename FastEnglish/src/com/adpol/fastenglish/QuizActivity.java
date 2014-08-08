4d05684823418bb39c30b871346dea9eb514aa09
kamilsherco <kamilsherco@gmail.com>
2014-08-08 16:15:23 +0200

    added quiz v.1

--- FastEnglish/src/com/adpol/fastenglish/QuizActivity.java
+++ FastEnglish/src/com/adpol/fastenglish/QuizActivity.java
@@ -1,10 +1,16 @@
 package com.adpol.fastenglish;
 
+import java.util.List;
+import java.util.Random;
+
+import com.adpol.fastenglish.database.DatabaseManager;
+import com.adpol.fastenglish.database.Word;
 import com.example.fastenglish.R;
 
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -17,17 +23,28 @@ public class QuizActivity extends Activity implements OnClickListener  {
     private ImageView answerC;
     private ImageView answerD;
     private TextView word;
+    private TextView answerAtxt;
+    private TextView answerBtxt;
+    private TextView answerCtxt;
+    private TextView answerDtxt;
+    
+    private int index;
+    private List<Word> wordsList;
+    
+    
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
 		initialize();
+		setTextAnswer();
 	}
 	
 	void initialize()
     {
 
-   
+		wordsList = DatabaseManager.getInstance().getAllWords();
+		
         answerA = (ImageView) findViewById(R.id.iQuizAnswerA);
         answerA.setOnClickListener(this);
        
@@ -42,9 +59,34 @@ public class QuizActivity extends Activity implements OnClickListener  {
         
         word = (TextView) findViewById(R.id.tvQuizWord);
         
+        answerAtxt = (TextView) findViewById(R.id.iQuizAnswerAtxt);
+        answerBtxt = (TextView) findViewById(R.id.iQuizAnswerBtxt);
+        answerCtxt = (TextView) findViewById(R.id.iQuizAnswerCtxt);
+        answerDtxt = (TextView) findViewById(R.id.iQuizAnswerDtxt);
+        
+        
+        randomText(word);
+        
 
 
     }
+	
+	private void randomText(TextView answer){
+		index = new Random().nextInt(wordsList.size());
+		answer.setText(wordsList.get(index).getEnWord());
+		
+	}
+	
+	private void setTextAnswer()
+	{
+		randomText(answerAtxt);
+		randomText(answerBtxt);
+		randomText(answerCtxt);
+		randomText(answerDtxt);
+		
+	}
+	
+	
 
 
     @Override
@@ -53,23 +95,64 @@ public class QuizActivity extends Activity implements OnClickListener  {
         switch(arg0.getId())
         {
         case R.id.iQuizAnswerA:
-     
+        	
+        	if(answerAtxt.getText().toString().equals(word.getText().toString()))
+        	{
+        		answerA.setImageResource(R.drawable.btcorrect);
+        	
+        	}
+        	else
+        	{
+        		answerA.setImageResource(R.drawable.btfail);
+        	
+        	}
        
 
         break;
         case R.id.iQuizAnswerB:
+        	if(answerBtxt.getText().toString().equals(word.getText().toString()))
+        	{
+        		answerB.setImageResource(R.drawable.btcorrect);
+        	
+        	}
+        	else
+        	{
+        		answerB.setImageResource(R.drawable.btfail);
+        	
+        	}
             
 
             break;
         case R.id.iQuizAnswerC:
+        	if(answerCtxt.getText().toString().equals(word.getText().toString()))
+        	{
+        		answerC.setImageResource(R.drawable.btcorrect);
+        	
+        	}
+        	else
+        	{
+        		answerC.setImageResource(R.drawable.btfail);
+        	
+        	}
         	  
             
 
 
             break;
         case R.id.iQuizAnswerD:
+        	if(answerDtxt.getText().toString().equals(word.getText().toString()))
+        	{
+        		answerD.setImageResource(R.drawable.btcorrect);
         	
-
+        	}
+        	else
+        	{
+        		answerD.setImageResource(R.drawable.btfail);
+        	
+        	}
+        	
+        	
+        	break;
         }
 
     }
